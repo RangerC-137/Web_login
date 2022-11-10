185da198d30b5dcb0896297410e7a95663380f56
1833530145qqcom <115228141+1833530145qqcom@users.noreply.github.com>
2022-11-10 09:48:04 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory()(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Login',
-      component: ()=>('../views/Login.vue')
-    },
-    {
-      path: '/home/:id',
-      name: 'home',
-      component: ()=>('../views/HomeView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'Login',
+            component: () => import('../views/Login.vue')
+        },
+        {
+            path: '/home/:id',
+            name: 'home',
+            component: () => import('../views/HomeView.vue')
+        }
+    ]
 })
 
 export default router
